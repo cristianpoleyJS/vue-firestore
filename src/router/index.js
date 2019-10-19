be09fcfb654092cb40ebb504dcae7c9dadcfed17
Cristian Poley <cripolgon@gmail.com>
2019-10-19 22:19:02 +0200

    [feat] Added all components, views and styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,17 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import ListArticles from '../views/ListArticles.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
+export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'ListArticles',
+      component: ListArticles
+    }
+  ]
 })
-
-export default router
